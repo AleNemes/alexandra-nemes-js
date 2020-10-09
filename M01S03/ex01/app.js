6b77ca09dc23bd1168df04ac537ed14aeb04fefa
AleNemes <64965844+AleNemes@users.noreply.github.com>
2020-10-09 16:14:33 +0300

    Tema 8 Octombrie

--- M01S03/ex01/app.js
+++ M01S03/ex01/app.js
@@ -15,3 +15,12 @@ console.log(
 // care ai trecut-o in obiect scazand din anul curent
 // (un proces numit hardcodare)
 console.log(2020 - object.age / 2);
+
+// Afiseaza fraza “xxx yyy are xx ani.” in
+// consola folosind operatorul de concatenare
+console.log(
+  object.name + ' ' + object.surname + ' are' + ' ' + object.age + ' de ani',
+);
+
+// Afiseaza anul in care te-ai nascut scazand varsta din anul curent
+console.log(2020 - 31);
