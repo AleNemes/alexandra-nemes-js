48c97d27a0c4a1ecf0e2b1d71ec6769f046102e3
AleNemes <64965844+AleNemes@users.noreply.github.com>
2020-10-13 21:58:27 +0300

    Curs 03

--- M01S03/ex06/app.js
+++ M01S03/ex06/app.js
@@ -1,6 +1,14 @@
+let name = 'Steven';
+
 console.log(`
-    Dragos este instructor de programare. Dragos studiaza JavaScript de la 17 ani. 
-    Cand Dragos a absolvit facultatea si-a facut licenta in JavaScript. 
-    Dragos are si un master in tehnologii internet. 
-    Dragos este instructor Pixellab si preda JavaScript acolo.
+  ${name} este instructor de programare. ${name} studiaza JavaScript de la 17 ani.
+  Cand ${name} a absolvit facultatea si-a facut licenta in JavaScript.
+  ${name} are si un master in tehnologii internet.
+  ${name} este instructor Pixellab si preda JavaScript acolo.
 `);
+
+// Folosind codul de la ex 06 schimba numele ${name} cu numele tau.
+
+// // Folosind o variabila numita name in care sa iti stochezi numele schimba textul astfel
+// incat propozitia sa afiseze numele tau.
+// Pentru a interpola o variabila in template string foloseste notatia ${numeVariabila}
