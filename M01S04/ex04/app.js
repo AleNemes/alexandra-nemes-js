fcf2f8bd85db30d197bc97bb1c29c25674acea64
AleNemes <64965844+AleNemes@users.noreply.github.com>
2020-10-15 22:00:35 +0300

    Curs 04

---
+++ M01S04/ex04/app.js
@@ -0,0 +1,29 @@
+// var i;
+
+// for (i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+var i;
+
+// for (i = 1; i < 100; i++) {
+//   console.log(i);
+
+//   if (i === 50) {
+//     break;
+//   }
+// }
+
+// for (i = 1; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+for (i = 1; i <= 100; i++) {
+  if (i % 5 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
