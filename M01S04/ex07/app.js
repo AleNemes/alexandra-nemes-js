fbca1fa24ab20a3ac900350521befdccee8dda5c
AleNemes <64965844+AleNemes@users.noreply.github.com>
2020-10-20 22:01:51 +0300

    Curs 04

---
+++ M01S04/ex07/app.js
@@ -0,0 +1,26 @@
+let friends = [
+  {
+    name: 'Dragos',
+    surname: 'Iordache',
+  },
+  {
+    name: 'Larry',
+    surname: 'Larryson',
+  },
+  {
+    name: 'Steven',
+    surname: 'Steveson',
+  },
+  {
+    name: 'Carol',
+    surname: 'Carolson',
+  },
+  {
+    name: 'Andra',
+    surname: 'Andrason',
+  },
+  {
+    name: 'Rangar',
+    surname: 'Rangarson',
+  },
+];
