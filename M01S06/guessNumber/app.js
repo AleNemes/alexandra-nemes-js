1732a0c3318f6d1539eb6afd5fe51fed69de9887
AleNemes <64965844+AleNemes@users.noreply.github.com>
2020-11-11 12:43:05 +0200

    Guess Number

--- M01S06/guessNumber/app.js
+++ M01S06/guessNumber/app.js
@@ -1,18 +1,25 @@
-let btnStart = document.getElementById('start');
-let btnReset = document.getElementById('reset');
-let btnCheck = document.getElementById('btnCheck');
-let output = document.getElementById('outPutText');
+let randomNumber = Math.floor(Math.random() * 100 + 1);
+let submitButton = document.getElementById('submitGuess');
+let newGameButton = document.getElementById('newGame');
+let result = document.getElementById('result');
 
-let number = Math.floor(Math.random() * 101);
+submitButton.addEventListener('click', function () {
+  let userGuess = document.getElementById('guessField').value;
 
-btnCheck.addEventListener('click', function () {
-  let input = document.getElementById('userInput').value;
-
-  if (input === number) {
-    output.innerText = `You guessed right, your number was ${number}`;
-  } else if (input < number) {
-    output.innerText = `Yo're suck! Your number is too low!`;
-  } else if (input > number) {
-    output.innerText = `Yo're suck! Your number is too high!`;
+  if (userGuess === randomNumber) {
+    result.innerText = 'You Win!!!';
+    result.style.color = '#1abc9a';
+  } else if (userGuess > randomNumber) {
+    result.innerText = 'Your number is too high! Try again!';
+  } else {
+    result.innerText = 'Your number is too low! Try again!';
+    result.style.color = '#1abc9a';
   }
+});
+
+newGameButton.addEventListener('click', function () {
+  result.innerText = 'Let`s play again!';
+  result.style.color = '#9b59b6';
+  document.getElementById('guessField').value = '';
+  randomNumber = Math.floor(Math.random() * 100 + 1);
 });
