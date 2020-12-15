3519fd03fe26bc69a3563822050fad03751fa91f
AleNemes <64965844+AleNemes@users.noreply.github.com>
2020-12-15 21:00:31 +0200

    mono

---
+++ Monochrome/js/app.js
@@ -0,0 +1,18 @@
+// ^ undeva mai sus este pristine
+let lookbookPage = document.querySelector('.monochrome-lookbook');
+
+if (lookbookPage !== null) {
+  const formClass = 'form-lookbook';
+  const form = document.querySelector(`.${formClass}`);
+  let pristine = null;
+
+  if (form !== null) {
+    pristine = new Pristine(form);
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      pristine.validate();
+    });
+  }
+}
