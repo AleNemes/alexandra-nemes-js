d8500ce3ecd3042b21810bd49de3b06b077d9d08
AleNemes <64965844+AleNemes@users.noreply.github.com>
2020-12-15 21:39:37 +0200

    Monochrome

--- Monochrome/js/app.js
+++ Monochrome/js/app.js
@@ -16,3 +16,15 @@ if (lookbookPage !== null) {
     });
   }
 }
+
+// j query
+// $(() => {
+//   let $page = $('.monochrome-lookbook');
+//   if ($page.length > 0) {
+//     const $form = $('.form-lookbook');
+
+//     if ($form.length > 0) {
+//       $form.validate();
+//     }
+//   }
+// });
